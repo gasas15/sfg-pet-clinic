1cab63a6bdb32f94e1b9601faf6726b61f9e0eed
Saimir Gasa <saimir.gasa@gmail.com>
2020-11-25 10:19:25 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/saimirgasa/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.saimirgasa.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/com/saimirgasa/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.saimirgasa.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
