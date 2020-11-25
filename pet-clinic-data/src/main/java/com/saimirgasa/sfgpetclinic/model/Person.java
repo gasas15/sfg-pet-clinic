af5e7445fd8b2fd4476d70bfb28993301df5c643
Saimir Gasa <saimir.gasa@gmail.com>
2020-11-25 11:57:14 +0100

    Adding BaseEntity. Closes #10

--- pet-clinic-data/src/main/java/com/saimirgasa/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/saimirgasa/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.saimirgasa.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/saimirgasa/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/saimirgasa/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.saimirgasa.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
